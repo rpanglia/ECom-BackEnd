a0f63f623c8e7df35bb959570f3d0f02aee37147
Ravneet Panglia <ravneet.panglia@gmail.com>
2021-12-30 17:52:41 -0500

    sequelize connection

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,7 +10,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
+// sequelize models sync to the database, then server turns on
 app.listen(PORT, () => {
+  sequelize.sync(() => {
+    console.log('Connected to the database!');
+  })
   console.log(`App listening on port ${PORT}!`);
 });
